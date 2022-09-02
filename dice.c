4f3da4fc107b6a70d4106dfc35ce8020b699b9b8
Lis0v <kokobolo@tutanota.com>
2022-09-02 22:50:00 +0200

    balls

---
+++ dice.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(){
+    srand(time(NULL));
+    int n;
+
+    for(int i=0;i<5;i++){
+        n = rand() % 6 + 1;
+        printf("%d ",n);
+    }
+    // dziaa
+}
